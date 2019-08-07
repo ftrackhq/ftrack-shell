3c6466bb3a742c54586eeb84b67cc591dc84ccba
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-08-07 15:43:06 +0200

    add first step for autocomplete

--- fcmd.py
+++ fcmd.py
@@ -1,6 +1,7 @@
 from cmd import Cmd
 from ftrack_api import Session
 from pprint import pformat
+import logging
 
 
 class FPrompt(Cmd):
@@ -13,39 +14,78 @@ class FPrompt(Cmd):
     @property
     def session(self):
         return self._session
-
-    def __init__(self, *args, **kwargs):
-        self._session = kwargs.pop('session')
-        Cmd.__init__(self, *args, **kwargs)
-        self._current_entity = None
-        self._listed_entities = {}
     
-    def do_ls(self, *args):
+    @property
+    def projects(self):
+        projects = self.session.query('select name from Project').all()
+        self._listed_entities = projects
+        return projects
+
+    @property
+    def children(self):
+        children = self._current_entity['children']
+        self._listed_entities = children
+        return children
+
+    def __init__(self, session=None):
+        self._session = session
+        Cmd.__init__(self)
+        self._current_entity = None
+        self._listed_entities = []
+        self.logger = logging.getLogger(self.__class__.__name__)
+    
+    def do_ls(self, line):
         if not self.current:
-            projects = self.session.query('select name from Project').all()
-            self._listed_entities = projects
+            projects = self.projects
             project_names = [p['name'] for p in projects]
             for project_name in project_names:
                 print project_name
         else:
-            entities = self._current_entity['children']
-            self._listed_entities = entities
+            entities = self.children
             entity_names = [e['name'] for e in entities]
             for entity_name in entity_names:
                 print entity_name
 
+    def complete_cd(self, text, line, start_index, end_index):
+
+        if not self.current:
+            projects = self.projects
+            project_names = [project['name'] for project in projects]
+            if not text:
+                return map(str, project_names[:])
+                
+            else:
+                return map(str, [
+                    project_name for project_name in project_names
+                    if project_name.startswith(text)
+                ])
+
+        else:
+            entities = self.children
+            entity_names = [entity['name'] for entity in entities]
+            if not text:
+                return entity_names[:]
+                
+            else:
+                return [
+                    entity_name for entity_name in entity_names 
+                    if entity_name.startswith(text)
+                ]
+                
     def do_quit(self, args):
         """Quits the program."""
         print "Quitting."
         raise SystemExit
 
-    def do_cd(self, *args):
+    def do_cd(self, line):
         current = [
             entity for entity in self._listed_entities
-            if entity['name'] == args[0]
+            if entity['name'] == line
         ][0]
         self._current_entity = current
-
+  
+    def do_EOF(self, line):
+        return True
 
 if __name__ == '__main__':
     session = Session()
